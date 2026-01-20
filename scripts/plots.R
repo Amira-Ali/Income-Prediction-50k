64406d180767cbb12a52aefae54a29e4e81f0771
Amira Shlebik <shlebikamira@gmail.com>
2026-01-20 14:22:03 +0000

    Update plots.R

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,14 +1,37 @@
 library(ggplot2)
+library(patchwork)
 
-make_model_comparison_plot <- function(results_df) {
-  ggplot(results_df, aes(x = feature_set, y = AUC, fill = model)) +
-    geom_col(position = "dodge") +
-    coord_flip() +
-    theme_minimal() +
-    labs(title = "Model Comparison by Feature Set",
-         x = "Feature Engineering Strategy",
-         y = "AUC")
+make_univariate_plots <- function(df, max_cols = 3) {
+  plots <- list()
+
+  num_cols <- names(df)[sapply(df, is.numeric)]
+  for (col_name in num_cols) {
+    p <- ggplot(df, aes(x = .data[[col_name]])) +
+      geom_histogram(bins = 30, fill = "#66C2C2") +
+      theme_minimal() +
+      labs(x = col_name, y = NULL)
+
+    plots[[length(plots) + 1]] <- p
+  }
+
+  cat_cols <- names(df)[sapply(df, function(x) is.character(x) || is.factor(x))]
+  for (col_name in cat_cols) {
+    p <- ggplot(df, aes(x = .data[[col_name]])) +
+      geom_bar(fill = "#66C2C2") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      labs(x = col_name, y = NULL)
+
+    plots[[length(plots) + 1]] <- p
+  }
+
+  if (length(plots) > 0) {
+    return(wrap_plots(plots, ncol = min(max_cols, length(plots))))
+  } else {
+    return(NULL)
+  }
 }
+
 
 # in rmd I should change every code for creating plots to a code like this
 # this way, rmd will be only presentation layer
